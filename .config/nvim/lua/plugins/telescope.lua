f1bf771c33bccebff01b9b2e5e0ee8e361054554
dylanhitt <dylan.hitt1@gmail.com>
2024-04-05 12:29:53 -0400

    chore: auto update telesope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
             { mode = "n", 'fg', "<cmd>Telescope live_grep<cr>",                 desc = "Live grep" },
